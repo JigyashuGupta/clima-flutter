1e625c9b4618de18b743b28d7ddb689da1b81725
JigyashuGupta <jigyashuguptaa@gmail.com>
2022-06-03 20:14:22 +0530

    Clima Project Completed

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,29 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const kAPIkey ='ad17da82bf8a73323c77c5a884f43512';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName){
+    var url = '$openWeatherMapURL?q=$cityName&appid=$kAPIkey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = networkHelper.getData();
+    return weatherData;
+
+  }
+
+  Future<dynamic> getLocationWeather() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kAPIkey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -18,3 +18,19 @@ const kButtonTextStyle = TextStyle(
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
+
+const ktextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(Icons.location_city),
+  hintText: 'Enter city Name',
+  hintStyle: TextStyle(
+    color:  Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
